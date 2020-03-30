fcceeb09c67987096ffbf3a346fb3ade98702dcb
marcus@NOTEBOOK <marcus11.04@web.de>
2020-03-30 11:20:29 +0200

    210. Redirecting Requests with react-router-dom.Redirect inside Switch.Component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 // import axios from 'axios';
 import Posts from "./Posts/Posts";
 import NewPost from "../../containers/Blog/NewPost/NewPost";
-import { Route, NavLink, Switch } from "react-router-dom";
+import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 
 import "./Blog.css";
 
@@ -55,10 +55,14 @@ class Blog extends Component {
         </header>
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/" render={() => <h1>Home 2</h1>} /> 
-        Note: Route.Parameter parsed from top to bottom, so ordering is importand!*/}
+        Note: Route.Parameter parsed from top to bottom, so ordering is importand!
+        Note: inside Switch use react-router.dom.Redirect, for navigate from.routes -> to.routes
+        - outside Switch only redirect to.routes workes! */}
         <Switch>
           <Route path="/new-post" component={NewPost} />
           <Route path="/posts" component={Posts} />
+          <Redirect from="/" to="/posts" />
+          {/* <Route path="/" component={Posts} /> */}
         </Switch>
       </div>
     );
