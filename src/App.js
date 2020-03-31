fa16472bc4591d3bec64d05ddd19de7969a75373
marcus@NOTEBOOK <marcus11.04@web.de>
2020-03-31 07:43:13 +0200

    example for th case, if the app running from a Server with set a BasePath

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
-import Blog from './containers/Blog/Blog';
+import Blog from "./containers/Blog/Blog";
 
+/**
+ * for serving your app from a Server.... example.com/myapp,
+ * You need to set the basepath for the react router!
+ * --> return {<BrowserRouter basename="/my-app">}
+ */
 class App extends Component {
   render() {
     return (
+      // <BrowserRouter basename="/my-app">
       <BrowserRouter>
         <div className="App">
           <Blog />
